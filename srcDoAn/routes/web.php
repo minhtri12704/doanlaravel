ea7589175b5d7436b835a5c53f1ae2170c7e5f60
QuangP03 <phamhuyminhquang310@gmail.com>
2025-04-23 21:14:57 +0700

    Category

--- srcDoAn/routes/web.php
+++ srcDoAn/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryCrudController;
+use App\Http\Controllers\BaiVietController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//route category
+Route::get('/categories', [CategoryCrudController::class, 'index'])->name('categories.index');
+Route::get('/categories/{id}/edit', [CategoryCrudController::class, 'editCategory'])->name('categories.editCategory');
+Route::delete('/categories/{id}', [CategoryCrudController::class, 'deleteCategory'])->name('categories.deleteCategory');
+Route::get('/categories/create', [CategoryCrudController::class, 'create'])->name('categories.create');
+Route::post('/categories', [CategoryCrudController::class, 'store'])->name('categories.store');
+Route::put('/categories/{id}', [CategoryCrudController::class, 'update'])->name('categories.update');
+
+//route tintuc
+//Route::get('/blog', [BaiVietController::class, 'index'])->name('baiviet.index');
+//Route::get('/blog/{id}', [BaiVietController::class, 'show'])->name('baiviet.show');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
